9959abf63aecc4f7aba0cbc48b0a67306884dd6c
garden627 <garden627@yahoo.co.jp>
2022-01-30 21:28:57 +0900

    SinglePost axios

--- client/src/components/singlePost/SinglePost.js
+++ client/src/components/singlePost/SinglePost.js
@@ -33,7 +33,8 @@ export default function SinglePost() {
   
   const handleDelete = async () => {
     try {
-      await axios.delete(`/posts/${post._id}`, {
+      await axiosInstance.delete(`/posts/${post._id}`, {
+      // await axios.delete(`/posts/${post._id}`, {
         data: { username: user.username }
       })
       window.location.replace("/")
@@ -42,7 +43,8 @@ export default function SinglePost() {
 
   const handleUpdate = async () => {
     try {
-      await axios.put(`/posts/${post._id}`, {
+      await axiosInstance.put(`/posts/${post._id}`, {
+      // await axios.put(`/posts/${post._id}`, {
         username: user.username, title: title, desc: desc
       })
       // window.location.reload()
